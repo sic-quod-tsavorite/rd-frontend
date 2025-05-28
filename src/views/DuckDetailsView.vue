25809f506e00321a962970977557f47619faa38c
Tsavorite <56306066+sic-quod-tsavorite@users.noreply.github.com>
2025-05-28 09:52:49 +0200

    array issue fix

--- src/views/DuckDetailsView.vue
+++ src/views/DuckDetailsView.vue
@@ -1,15 +1,15 @@
 <template>
-  <div v-if="duck && duck.length > 0" class="flex flex-wrap">
+  <div v-if="duck" class="flex flex-wrap">
     <div class="w-1/2 p-4">
       <img
         class="w-full rounded-lg object-cover h-full"
-        :src="duck[0].imageURL"
+        :src="duck.imageURL"
         alt=""
       />
     </div>
     <div class="w-1/2 p-4">
-      <h2 class="text-2xl font-bold mb-4">{{ duck[0].name }}</h2>
-      <p class="text-slate-200 mb-4">{{ duck[0].description }}</p>
+      <h2 class="text-2xl font-bold mb-4">{{ duck.name }}</h2>
+      <p class="text-slate-200 mb-4">{{ duck.description }}</p>
     </div>
   </div>
   <div v-else>Loading duck details...</div>
@@ -22,7 +22,7 @@ import { useDucks } from "../modules/useDucks";
 import type { Duck } from "../interfaces/interfaces";
 
 const route = useRoute();
-const duck = ref<Duck[] | null>(null);
+const duck = ref<Duck | null>(null);
 
 const { fetchDuckById } = useDucks();
 
@@ -30,7 +30,7 @@ onMounted(async () => {
   const duckId = route.params.id as string;
   const fetchedDuck = await fetchDuckById(duckId);
   //console.log("fetched duck: ", fetchedDuck);
-  duck.value = fetchedDuck;
+  duck.value = Array.isArray(fetchedDuck) ? fetchedDuck[0] ?? null : fetchedDuck;
   //console.log("duck: ", duck.value);
 });
 </script>
